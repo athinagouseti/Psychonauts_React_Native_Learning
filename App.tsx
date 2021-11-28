ea41b05af289bedca2abcbf2503e3dcb315aa738
Athina Gouseti <athina.gouseti@outlook.com>
2021-11-28 20:02:33 +0000

    Added bottom tab navigator

--- App.tsx
+++ App.tsx
@@ -2,20 +2,34 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Homescreen from './components/homescreen/Homescreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Characters from './components/characters/Characters';
+import Favourites from './components/favourites/Favourites';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <>
       <StatusBar style="auto" />
-      <Homescreen /> 
-    </View>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={Homescreen} />
+          <Tab.Screen name="Characters" component={Characters} />
+          <Tab.Screen name="Favourites" component={Favourites} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    // backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ components/characters/Characters.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+
+const Characters: React.FC = ()=> {
+
+    return(
+        <View>
+            <Text>Characters</Text>
+        </View>
+    )
+}
+
+export default Characters;
